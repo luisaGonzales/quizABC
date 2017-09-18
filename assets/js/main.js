8455759ccde53162f10fddb45375e40f6d23df2f
luisaGonzales <luisagc711@gmail.com>
2017-09-18 03:46:09 -0500

    Cambio de lógica. Todas las preguntas

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,19 +8,20 @@ const app = {
         pregunta2: new Pregunta(2, `¿Cuál es el puerto más grande del mundo?`, ['Puerto de Shangai', 'Puerto de Singapur', 'Puerto de Rotterdam'], "Puerto de Shangai"),
         pregunta3: new Pregunta(3, `¿Cuál es la distancia más larga en bicicleta hacia atrás?`, ["89.30 km", "675.10 km", "337.60 km"], "337.60 km"),
         pregunta4: new Pregunta(4, `¿Cuál es la velocidad más alta alcanzada nunca por un autobús escolar?`, ["590 km/h", "320 km/h", "245 km/h"], "590 km/h"),
-        pregunta5: new Pregunta(5, `¿Cuál es el viaje más largo en un tanque de gas?`, ["2,617 km", "3,568 km", "1,732 km"], "2,617 km"),
+        pregunta5: new Pregunta(5, `¿Cuál es el viaje más largo con un tanque de gas?`, ["2,617 km", "3,568 km", "1,732 km"], "2,617 km"),
     },
     setup : function() {
         // Botón comenzar
         $("#comenzar").click(function(){
-            app.preguntaActual += 1;
             let inicio = $("#inicio").addClass("no-display");
             console.log(inicio);
-            ponerImagen(app.preguntaActual);
-            ponerEstado(app.preguntaActual);
+            // ponerImagen(app.preguntaActual);
+            // ponerEstado(app.preguntaActual);
+            $("#pregunta4").removeClass("no-display");
         });
     },
-    imagenes:["", "assets/img/plane.svg", "assets/img/ship.svg", "assets/img/bycicle.svg", "assets/img/bus.svg","assets/img/car.svg", "assets/img/truck.svg"],
+    imagenes:["assets/img/plane.svg", "assets/img/ship.svg", "assets/img/bycicle.svg", "assets/img/bus.svg","assets/img/car.svg", "assets/img/truck.svg"],
+    secciones: ["#pregunta1", "#pregunta2", "#pregunta3", "#pregunta4", "#pregunta5"]
 }
 
 // Función constructura
@@ -34,19 +35,19 @@ function Pregunta (numero, pregunta, alternativas, correcta) {
     }
 }
 
-function ponerImagen (num){    
-    let sectionImg = $("#imagen");
-    let imagen = $("<img>", {
-      "src": `${app.imagenes[num]}`,
-    });
-    sectionImg.append(imagen);
-}
+// function ponerImagen (num){    
+//     let sectionImg = $("#imagen");
+//     let imagen = $("<img>", {
+//       "src": `${app.imagenes[num]}`,
+//     });
+//     sectionImg.append(imagen);
+// }
 
-function ponerEstado (num){
-    let divEstado = $("#estado");
-    let estado = `${num} de 5 preguntas contestadas`
-    divEstado.append(estado);
-}
+// function ponerEstado (num){
+//     let divEstado = $("#estado");
+//     let estado = `${num} de 5 preguntas contestadas`
+//     divEstado.append(estado);
+// }
 
 $(document).ready(app.setup);
 
