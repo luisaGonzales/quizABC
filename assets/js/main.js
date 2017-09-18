3970ba55ea5354087d11f595bf229a108dc5edc2
luisaGonzales <luisagc711@gmail.com>
2017-09-18 04:37:11 -0500

    Recuperación de respuestas marcadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@
 // Objeto de preguntas
 const app = {
     preguntaActual: 0,
+    respuestas: [undefined, undefined, undefined, undefined, undefined],
     preguntas: {
         pregunta1: new Pregunta(1, `¿Cuál es la aerolínea más antigua del mundo?`, ["Avianca", "KLM", "Qantas"], "KML"),
         pregunta2: new Pregunta(2, `¿Cuál es el puerto más grande del mundo?`, ['Puerto de Shangai', 'Puerto de Singapur', 'Puerto de Rotterdam'], "Puerto de Shangai"),
@@ -10,23 +11,29 @@ const app = {
         pregunta4: new Pregunta(4, `¿Cuál es la velocidad más alta alcanzada nunca por un autobús escolar?`, ["590 km/h", "320 km/h", "245 km/h"], "590 km/h"),
         pregunta5: new Pregunta(5, `¿Cuál es el viaje más largo con un tanque de gas?`, ["2,617 km", "3,568 km", "1,732 km"], "2,617 km"),
     },
-    setup : function() {
+    setup: function () {
         // Botón comenzar
-        $("#comenzar").click(function(){
+        $("#comenzar").click(function () {
             app.preguntaActual += 1;
             let inicio = $("#inicio").addClass("no-display");
             app.mostrarPregunta(app.preguntaActual);
         });
-        $().click()
     },
     secciones: ["0", "pregunta1", "pregunta2", "pregunta3", "pregunta4", "pregunta5"],
-    mostrarPregunta : function(preguntaActual){
-        $(`#${app.secciones[preguntaActual]}`).removeClass("no-display");
-    }
+    mostrarPregunta: function (sgtPregunta) {
+        $(`#${app.secciones[sgtPregunta]}`).removeClass("no-display");
+    },
+    siguientePregunta: function () {
+        $(`#${app.secciones[app.preguntaActual]}`).addClass("no-display");
+        respuestaMarcada();
+        app.preguntaActual += 1;
+        $(`#${app.secciones[app.preguntaActual]}`).removeClass("no-display");
+
+    },
 }
 
 // Función constructura
-function Pregunta (numero, pregunta, alternativas, correcta) {
+function Pregunta(numero, pregunta, alternativas, correcta) {
     this.numero = numero;
     this.pregunta = pregunta;
     this.opciones = {
@@ -35,5 +42,12 @@ function Pregunta (numero, pregunta, alternativas, correcta) {
     }
 }
 
-$(document).ready(app.setup);
+function respuestaMarcada() {
+    $(`#${app.preguntaActual}`).click(function (e) {
+        let respuestaID = e.target.id;
+        app.respuestas[app.preguntaActual - 1] = respuestaID;
+        console.log(respuestaID);
+    });
+}
 
+$(document).ready(app.setup);
