6780d4abf72280088a8955fff87b4990afe53325
luisaGonzales <luisagc711@gmail.com>
2017-09-17 22:20:37 -0500

    Función constructura,función que crea imagen, preguntas construidas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,50 @@
 'use strict';
 
-// Inicio
+// Objeto de preguntas
+const app = {
+    preguntaActual: 0,
+    preguntas: {
+        pregunta1: new Pregunta(1, `¿Cuál es la aerolínea más antigua del mundo?`, ["Avianca", "KLM", "Qantas"], "KML"),
+        pregunta2: new Pregunta(2, `¿Cuál es el puerto más grande del mundo?`, ['Puerto de Shangai', 'Puerto de Singapur', 'Puerto de Rotterdam'], "Puerto de Shangai"),
+        pregunta3: new Pregunta(3, `¿Cuál es la distancia más larga en bicicleta hacia atrás?`, ["89.30 km", "675.10 km", "337.60 km"], "337.60 km"),
+        pregunta4: new Pregunta(4, `¿Cuál es la velocidad más alta alcanzada nunca por un autobús escolar?`, ["590 km/h", "320 km/h", "245 km/h"], "590 km/h"),
+        pregunta5: new Pregunta(5, `¿Cuál es el viaje más largo en un tanque de gas?`, ["2,617 km", "3,568 km", "1,732 km"], "2,617 km"),
+    },
+    setup : function() {
+        // Botón comenzar
+        $("#comenzar").click(function(){
+            app.preguntaActual += 1;
+            let inicio = $("#inicio").addClass("no-display");
+            console.log(inicio);
+            crearQuiz(app.preguntaActual);
+        });
+    },
+    imagenes:["", "assets/img/plane.svg", "assets/img/ship.svg", "assets/img/bycicle.svg", "assets/img/bus.svg","assets/img/car.svg", "assets/img/truck.svg"],
+}
 
-$("#comenzar").click(function(){
-    let inicio = $("#inicio").addClass("no-display");
-    console.log(inicio);
-});
+// Función constructura
+
+function Pregunta (numero, pregunta, alternativas, correcta) {
+    this.numero = numero;
+    this.pregunta = pregunta;
+    this.opciones = {
+        alternativas: alternativas,
+        correcta: correcta
+    }
+}
+
+function crearQuiz (num){    
+    let sectionImg = $("#imagen");
+    let imagen = $("<img>", {
+      "src": `${app.imagenes[num]}`,
+    });
+    sectionImg.append(imagen);
+}
+
+
+
+
+
+
+$(document).ready(app.setup);
+
